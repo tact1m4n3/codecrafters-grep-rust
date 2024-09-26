6039403d1be2d129358237290a14b61e5a6c3217
tact1m4n3 <tudor@dicu.org>
2024-09-26 22:08:16 +0300

    refactor and either

--- src/main.rs
+++ src/main.rs
@@ -32,18 +32,21 @@ fn run() -> anyhow::Result<bool> {
             .read_line(&mut input_line)
             .context("reading input")?;
 
-        let pattern = Pattern::parse(&pattern)?;
-        // println!("{:?}", pattern);
-
-        let mut input_iter = input_line.chars().enumerate().peekable();
-        while input_iter.peek() != None {
-            if pattern.matches(&mut input_iter) {
-                return Ok(true);
-            } else {
-                input_iter.next();
+        if let Some(pattern) = Pattern::parse_either(&mut pattern.chars().peekable(), EndFlags::empty())? {
+            println!("{pattern:?}");
+            let mut input_iter = input_line.chars().enumerate().peekable();
+            while input_iter.peek() != None {
+                if pattern.matches(&mut input_iter) {
+                    return Ok(true);
+                } else {
+                    input_iter.next();
+                }
             }
+
+            Ok(false)
+        } else {
+            Ok(true)
         }
-        Ok(false)
     } else {
         anyhow::bail!("No pattern provided.");
     }
@@ -66,32 +69,111 @@ where
     }
 }
 
-type InputIter<'a> = std::iter::Peekable<std::iter::Enumerate<std::str::Chars<'a>>>;
+bitflags::bitflags! {
+    #[derive(Clone, Copy)]
+    struct EndFlags: u8 {
+        const RPAREN = 1 << 0;
+        const PIPE = 1 << 1;
+    }
+}
 
-#[derive(Debug)]
-struct Pattern {
-    items: Vec<PatternItem>,
+type InputIter<'a> = std::iter::Peekable<std::iter::Enumerate<std::str::Chars<'a>>>;
+type PatternIter<'a> = std::iter::Peekable<std::str::Chars<'a>>;
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+enum Pattern {
+    Literal(char),
+    Digit,
+    Alphanumeric,
+    CharacterGroup { positive: bool, group: String },
+    StartAnchor,
+    EndAnchor,
+    OneOrMore(Box<Pattern>),
+    ZeroOrOne(Box<Pattern>),
+    Wildcard,
+    List(Vec<Pattern>),
+    Either(Vec<Pattern>),
 }
 
 impl Pattern {
-    pub fn parse(raw: &str) -> anyhow::Result<Self> {
-        let mut items = Vec::new();
+    pub fn parse_either(iter: &mut PatternIter, end: EndFlags) -> anyhow::Result<Option<Self>> {
+        let mut pattern = None;
 
-        let mut chars = raw.chars().peekable();
-        while let Some(c) = chars.next() {
-            let item = match c {
+        while let Some(item) = Self::parse_list(iter, end | EndFlags::PIPE)? {
+            pattern = if let Some(pattern) = pattern.take() {
+                if let Pattern::Either(mut items) = pattern {
+                    items.push(item);
+                    Some(Pattern::Either(items))
+                } else {
+                    let items = vec![pattern, item];
+                    Some(Pattern::Either(items))
+                }
+            } else {
+                Some(item)
+            };
+
+            if let Some(c) = iter.peek().copied() {
+                if c == '|' {
+                    iter.next();
+                } else {
+                    break;
+                }
+            }
+        }
+
+        Ok(pattern)
+    }
+
+    pub fn parse_list(iter: &mut PatternIter, end: EndFlags) -> anyhow::Result<Option<Self>> {
+        let mut pattern = None;
+
+        while let Some(item) = Self::parse_one(iter)? {
+            pattern = if let Some(pattern) = pattern.take() {
+                if let Pattern::List(mut items) = pattern {
+                    items.push(item);
+                    Some(Pattern::List(items))
+                } else {
+                    let items = vec![pattern, item];
+                    Some(Pattern::List(items))
+                }
+            } else {
+                Some(item)
+            };
+
+            if let Some(c) = iter.peek().copied() {
+                if c == ')' && end.contains(EndFlags::RPAREN) || c == '|' && end.contains(EndFlags::PIPE) {
+                    break;
+                }
+            }
+        }
+
+        Ok(pattern)
+    }
+
+    pub fn parse_one(iter: &mut PatternIter) -> anyhow::Result<Option<Self>> {
+        if let Some(c) = iter.next() {
+            let mut item = match c {
                 '\\' => {
-                    let c = chars.expect()?;
+                    let c = iter.expect()?;
                     match c {
-                        'd' => PatternItem::Digit,
-                        'w' => PatternItem::Alphanumeric,
-                        c => return Err(anyhow::anyhow!("expected d|w, got {}", c)),
+                        'd' => Pattern::Digit,
+                        'w' => Pattern::Alphanumeric,
+                        c => return Err(anyhow::anyhow!("expected 'd' or 'w', got '{}'", c)),
+                    }
+                }
+                '(' => {
+                    if let Some(item) = Self::parse_either(iter, EndFlags::RPAREN)? {
+                        let c = iter.expect()?;
+                        anyhow::ensure!(c == ')', "expected ')'");
+                        item
+                    } else {
+                        anyhow::bail!("empty group is not allowed");
                     }
                 }
                 '[' => {
                     let mut group = String::new();
 
-                    let c = chars.expect()?;
+                    let c = iter.expect()?;
                     let positive = if c == '^' {
                         false
                     } else {
@@ -100,66 +182,44 @@ impl Pattern {
                     };
 
                     loop {
-                        let c = chars.expect()?;
+                        let c = iter.expect()?;
                         if c == ']' {
                             break;
                         }
                         group.push(c);
                     }
 
-                    PatternItem::CharacterGroup { positive, group }
+                    Pattern::CharacterGroup { positive, group }
                 }
-                '^' => PatternItem::StartAnchor,
-                '$' => PatternItem::EndAnchor,
-                '+' => PatternItem::OneOrMore(Box::new(
-                    items
-                        .pop()
-                        .context("can't use '+' at the start of the pattern")?,
-                )),
-                '?' => PatternItem::ZeroOrOne(Box::new(
-                    items
-                        .pop()
-                        .context("can't use '?' at the start of the pattern")?,
-                )),
-                '.' => PatternItem::Wildcard,
-                c => PatternItem::Literal(c),
+                '^' => Pattern::StartAnchor,
+                '$' => Pattern::EndAnchor,
+                '+' => anyhow::bail!("can't use '+' at the start of the pattern"),
+                '?' => anyhow::bail!("can't use '+' at the start of the pattern"),
+                '.' => Pattern::Wildcard,
+                c => Pattern::Literal(c),
             };
 
-            items.push(item);
-        }
-
-        Ok(Self { items })
-    }
-
-    pub fn matches(&self, iter: &mut InputIter) -> bool {
-        for item in self.items.iter() {
-            if !item.matches(iter) {
-                return false;
+            while let Some(c) = iter.peek().copied() {
+                if c == '+' {
+                    item = Pattern::OneOrMore(Box::new(item))
+                } else if c == '?' {
+                    item = Pattern::ZeroOrOne(Box::new(item))
+                } else {
+                    break;
+                }
+                iter.next();
             }
+
+            Ok(Some(item))
+        } else {
+            Ok(None)
         }
-
-        true
     }
-}
 
-#[derive(Debug, PartialEq, Eq)]
-enum PatternItem {
-    Literal(char),
-    Digit,
-    Alphanumeric,
-    CharacterGroup { positive: bool, group: String },
-    StartAnchor,
-    EndAnchor,
-    OneOrMore(Box<PatternItem>),
-    ZeroOrOne(Box<PatternItem>),
-    Wildcard,
-}
-
-impl PatternItem {
     fn matches(&self, iter: &mut InputIter) -> bool {
         if let Some((i, c)) = iter.peek().copied() {
             match self {
-                PatternItem::Literal(expected) => {
+                Pattern::Literal(expected) => {
                     if *expected == c {
                         iter.next();
                         true
@@ -167,7 +227,7 @@ impl PatternItem {
                         false
                     }
                 }
-                PatternItem::Digit => {
+                Pattern::Digit => {
                     if c.is_digit(10) {
                         iter.next();
                         true
@@ -175,7 +235,7 @@ impl PatternItem {
                         false
                     }
                 }
-                PatternItem::Alphanumeric => {
+                Pattern::Alphanumeric => {
                     if c.is_alphanumeric() {
                         iter.next();
                         true
@@ -183,7 +243,7 @@ impl PatternItem {
                         false
                     }
                 }
-                PatternItem::CharacterGroup {
+                Pattern::CharacterGroup {
                     positive,
                     group: chars,
                 } => {
@@ -194,9 +254,9 @@ impl PatternItem {
                         false
                     }
                 }
-                PatternItem::StartAnchor => i == 0,
-                PatternItem::EndAnchor => false,
-                PatternItem::OneOrMore(inner) => {
+                Pattern::StartAnchor => i == 0,
+                Pattern::EndAnchor => false,
+                Pattern::OneOrMore(inner) => {
                     if !inner.matches(iter) {
                         false
                     } else {
@@ -204,17 +264,37 @@ impl PatternItem {
                         true
                     }
                 }
-                PatternItem::ZeroOrOne(inner) => {
+                Pattern::ZeroOrOne(inner) => {
                     inner.matches(iter);
                     true
                 }
-                PatternItem::Wildcard => {
+                Pattern::Wildcard => {
                     iter.next();
+                    true
+                }
+                Pattern::Either(items) => {
+                    for item in items.iter() {
+                        let mut tmp_iter = iter.clone();
+                        if item.matches(&mut tmp_iter) {
+                            *iter = tmp_iter;
+                            return true;
+                        }
+                    }
+
+                    false
+                }
+                Pattern::List(items) => {
+                    for item in items.iter() {
+                        if !item.matches(iter) {
+                            return false;
+                        }
+                    }
+
                     true
                 }
             }
         } else {
-            *self == PatternItem::EndAnchor
+            *self == Pattern::EndAnchor
         }
     }
 }
